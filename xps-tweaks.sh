53d3b392150c40835f8d9bfca2ed8cbd171e9888
Matteo Iervasi <matteoiervasi@gmail.com>
2019-09-15 13:49:22 +0200

    Update xps-tweaks.sh

--- xps-tweaks.sh
+++ xps-tweaks.sh
@@ -186,12 +186,6 @@ fi
 # Other packages
 apt -y install intel-microcode
 
-# Install wifi drivers
-rm -rf /lib/firmware/ath10k/QCA6174/hw3.0/*
-wget -O /lib/firmware/ath10k/QCA6174/hw3.0/board.bin https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/board.bin?raw=true
-wget -O /lib/firmware/ath10k/QCA6174/hw3.0/board-2.bin https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/board-2.bin?raw=true
-wget -O /lib/firmware/ath10k/QCA6174/hw3.0/firmware-4.bin https://github.com/kvalo/ath10k-firmware/blob/master/QCA6174/hw3.0/firmware-4.bin_WLAN.RM.2.0-00180-QCARMSWPZ-1?raw=true
-
 # Enable power saving tweaks for Intel chip
 if [[ $(uname -r) == *"4.15"* ]]; then
     echo "options i915 enable_fbc=1 enable_guc_loading=1 enable_guc_submission=1 disable_power_well=0 fastboot=1" > /etc/modprobe.d/i915.conf
